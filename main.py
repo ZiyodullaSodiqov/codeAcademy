53becdf64b161a1d8cd5151efc3e4b2b0a6e5057
ZiyodullaSodiqov <ziyodullasodiqov2007@gmail.com>
2025-05-12 16:14:13 +0500

    first commit

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 
 CORS(app, resources={
     r"/api/*": {
-        "origins": ["http://localhost:3000"],
+        "origins": ["https://codeacademy.nordicuniversity.org"],
         "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
         "allow_headers": ["Content-Type", "Authorization"],
         "supports_credentials": True,
